d4066951042cd49f26dddf737eaf65cac468505c
Elizaveta Gnedova <60923129+neNeptune@users.noreply.github.com>
2021-08-21 18:45:53 +0300

    Update caesar.c

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+//Encrypting the text using the Caesar cipher
+int main(int argc, char argv[])
+{
+    argc = 2;
+    printf("Please, enter k: ");
+    scanf ("%c", &argv[1]);
+    
+    int k = atoi(argv[1]);
+    printf ("%i", k);
+    if (k < 0){
+        return 1;
+    }
+    return 0;
+}
